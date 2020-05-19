038b9b4a6d447542c2c563a6272b103efca148d7
itaydnl <itaydnl1@gmail.com>
2020-05-19 18:42:30 +0300

    Updates

---
+++ precache-manifest.d19161e3ca1d24047fa1f959333b7e7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d605be5c3fca2aa3afff01d4a9fd5f42",
+    "url": "/Exchange/index.html"
+  },
+  {
+    "revision": "31a300e19d97b62d777d",
+    "url": "/Exchange/static/css/main.8198b2a5.chunk.css"
+  },
+  {
+    "revision": "3317a86d02e5cab892dd",
+    "url": "/Exchange/static/js/2.7e0da577.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Exchange/static/js/2.7e0da577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a300e19d97b62d777d",
+    "url": "/Exchange/static/js/main.daba26e1.chunk.js"
+  },
+  {
+    "revision": "15087f364b0c84e74a5c",
+    "url": "/Exchange/static/js/runtime-main.cd2a9c60.js"
+  }
+]);
